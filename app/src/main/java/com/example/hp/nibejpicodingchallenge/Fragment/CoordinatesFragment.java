bbe963e132c9d3bdf2b5258f446c01cd9d762d52
cthoughtz <cthoughtz@gmail.com>
2019-02-09 05:34:16 -0500

    created coordinatesFragment layout

---
+++ app/src/main/java/com/example/hp/nibejpicodingchallenge/Fragment/CoordinatesFragment.java
@@ -0,0 +1,30 @@
+package com.example.hp.nibejpicodingchallenge.Fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.hp.nibejpicodingchallenge.R;
+
+
+public class CoordinatesFragment extends Fragment {
+
+
+
+    public CoordinatesFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_coordinates, container, false);
+    }
+}
--- app/src/main/java/com/example/hp/nibejpicodingchallenge/MainActivity.java
+++ app/src/main/java/com/example/hp/nibejpicodingchallenge/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.hp.nibejpicodingchallenge;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.hp.nibejpicodingchallenge.Fragment.CoordinatesFragment;
+import com.example.hp.nibejpicodingchallenge.Fragment.ForecastFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    CoordinatesFragment coordinatesFragment = new CoordinatesFragment();
+    ForecastFragment forecastFragment = new ForecastFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragment_container, coordinatesFragment);
+        fragmentTransaction.commit();
+
     }
 }
